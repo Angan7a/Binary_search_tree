32bb52c7ba3dadde282cb2fd8a056ff0345ca336
Andrzej <angan@o2.pl>
2017-09-22 23:06:26 +0200

    Add some test, before merge

--- binary_serch_node.cpp
+++ binary_serch_node.cpp
@@ -143,6 +143,68 @@ public:
 	void printSpace(int a) {
 		while(a--) cout << " ";
 	}
+
+	void printTree1() {
+		int t=170;
+		int v[100][t];
+		char c[100][t];
+		int k=t/2;
+		int h=32;
+		Node* n;
+		for(int j=0; j<t; j++) {
+			for(int i=0; i<h; i++) {
+				if(v[i][j] != 0) {
+					v[i][j] = 0;
+				}
+			}
+		}
+		for(int j=0; j<t; j++) {
+			for(int i=0; i<h; i++) {
+				if(c[i][j] != 0) {
+					c[i][j] = 0;
+				}
+			}
+		}
+		v[1][k] = root->getValue();
+
+		for(int i=0; i<h; i++) {
+			k = k*0.7;
+			for(int j=0; j<t; j++) {
+				if(v[i][j] != 0) {
+					n = search(v[i][j], root)->getLeft();
+					if(n != NULL) {
+						for(int a=0; a<k; a++) c[i+1][j-a] = '-';
+						v[i+2][j-k] = n->getValue();
+					}
+					n = search(v[i][j], root)->getRight();
+					if(n != NULL) {
+						for(int a=0; a<k; a++) c[i+1][j+a] = '>';
+						v[i+2][j+k] = n->getValue();
+					}
+				}
+			}
+		}
+		for(int i=0; i<h; i++) {
+			for(int j=0; j<t; j++) {
+				if (i%2) {
+					if(v[i][j] != 0) {
+						cout << v[i][j];
+					} else {
+						cout << " ";
+					}
+				} else {
+					if(c[i][j] != 0) {
+						cout << c[i][j];
+					} else {
+						cout << " ";
+					}
+				}
+			}
+		cout << endl;
+		}
+
+	}
+
 	~Tree() {
 		if(!do_zniszczenia.empty()) {
 			for(vector<Node*>::reverse_iterator itr = do_zniszczenia.rbegin(); itr != do_zniszczenia.rend(); ++itr) {
@@ -157,27 +219,44 @@ int main()
 {
 	Tree t(29);
 
-	t.addNode(7);
-	t.addNode(6);
-	t.addNode(55);
-	t.addNode(10);
+	t.addNode(9);
+	t.addNode(36);
+	t.addNode(57);
+	t.addNode(13);
 	t.addNode(4);
 	t.addNode(1);
 	t.addNode(12);
 	t.addNode(15);
 	t.addNode(18);
-	t.addNode(13);
+
 	t.addNode(11);
-	t.addNode(9);
+	t.addNode(20);
 	t.addNode(54);
 	t.addNode(53);
 	t.addNode(51);
+	t.addNode(555);
+	t.addNode(550);
+	t.addNode(55);
+	t.addNode(56);
+	t.addNode(50);
+	t.addNode(14);
+	t.addNode(3);
+	t.addNode(6);
+	t.addNode(5);
+	t.addNode(8);
+	t.addNode(7);
+	t.addNode(13);
+	t.addNode(25);
+	t.addNode(22);
+	t.addNode(26);
+
 //	t.remove(18);
 
 	cout << "Tree has ";
-	if(t.search(5, t.root) == NULL) cout << "not ";
-	cout  << "number " << 5 << endl;
+	if(t.search(13, t.root) == NULL) cout << "not ";
+	cout  << "number " << 13 << endl;
 
+	t.printTree1();
 	t.printTree();
 return 0;
 }
